7e9548db752ccbe2f0e58fda611aa9fc8749a174
Maxim Bezrukavnikov <dmntr@rambler.ru>
2017-06-23 09:34:14 +1000

    Create SSC.user.js

---
+++ SSC.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Summer Sale Clicker
+// @namespace    SSC
+// @description Simple button Clicker
+// @version 0.01
+// @author DEMENT0R
+// @downloadURL https://github.com/DEMENT0R/steam_activation_button/raw/master/SSC.user.js
+// @updateURL https://github.com/DEMENT0R/steam_activation_button/raw/master/SSC.user.js
+// @license GNU v3
+// @noframes
+// @match        http://store.steampowered.com/*
+// @match        https://store.steampowered.com/*
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @grant         GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.querySelector('.next_in_queue_content').click();
+})();
