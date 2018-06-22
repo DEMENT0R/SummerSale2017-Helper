49265001820900cf274e054cf3398647b8adb7a9
DEMENTOR <dmntr@rambler.ru>
2018-06-23 00:36:32 +1000

    Update SSC.user.js

--- SSC.user.js
+++ SSC.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         Summer Sale 2017 Clicker
 // @namespace    SSC
-// @description Simple button Clicker
-// @version 0.03
-// @author DEMENT0R
-// @downloadURL https://github.com/DEMENT0R/SummerSale2017-Helper/raw/master/SSC.user.js
-// @updateURL https://github.com/DEMENT0R/SummerSale2017-Helper/raw/master/SSC.user.js
-// @license GNU v3
+// @description  Simple button Clicker
+// @version      0.04
+// @author       DEMENT0R
+// @downloadURL  https://github.com/DEMENT0R/SteamSaleHelper/raw/master/SSH.user.js
+// @updateURL    https://github.com/DEMENT0R/SteamSaleHelper/raw/master/SSH.user.js
+// @license      GNU v3
 // @noframes
 // @match        http://store.steampowered.com/*
 // @match        https://store.steampowered.com/*
-// @require  http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // @grant         GM_addStyle
 // ==/UserScript==
 
